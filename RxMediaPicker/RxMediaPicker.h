39a0053d93827043bea13e144f77450a0940d4d1
Rui Costa <rui.pfcosta@gmail.com>
2016-02-05 23:03:20 +0000

    Initial Commit

---
+++ RxMediaPicker/RxMediaPicker.h
@@ -0,0 +1,19 @@
+//
+//  RxMediaPicker.h
+//  RxMediaPicker
+//
+//  Created by Rui Costa on 05/02/2016.
+//  Copyright © 2016 Rui Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxMediaPicker.
+FOUNDATION_EXPORT double RxMediaPickerVersionNumber;
+
+//! Project version string for RxMediaPicker.
+FOUNDATION_EXPORT const unsigned char RxMediaPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxMediaPicker/PublicHeader.h>
+
+
