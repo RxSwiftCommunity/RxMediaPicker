8d9be5a7b53260a301ab9500e84b110f7f7509c8
Andrew Breckenridge <asbreckenridge@me.com>
2016-09-30 14:56:33 -0700

    update demo project

--- iOS Example/iOS Example/AppDelegate.swift
+++ iOS Example/iOS Example/AppDelegate.swift
@@ -5,6 +5,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    
+
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
